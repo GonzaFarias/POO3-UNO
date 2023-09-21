708ce23b4c6b0b24d2767ea322744488171af197
Gonzalo Farías <gonzaxeneize01@gmail.com>
2023-09-20 22:01:32 -0300

    Add files via upload

---
+++ TP_Patrones/P_Memento.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Patron_Memento
+{
+    // Clase Origen que representa el estado original que queremos guardar y restaurar.
+    class Origen
+    {
+        private string estado;
+
+        public Origen(string estadoInicial)
+        {
+            estado = estadoInicial;
+        }
+
+        public void SetEstado(string nuevoEstado)
+        {
+            estado = nuevoEstado;
+        }
+
+        public string GetEstado()
+        {
+            return estado;
+        }
+
+        public Memento Guardar()
+        {
+            return new Memento(estado);
+        }
+
+        public void Restaurar(Memento memento)
+        {
+            estado = memento.GetEstado();
+        }
+    }
+
+    // Clase Memento que representa el objeto que almacena el estado.
+    class Memento
+    {
+        private string estado;
+
+        public Memento(string estado)
+        {
+            this.estado = estado;
+        }
+
+        public string GetEstado()
+        {
+            return estado;
+        }
+    }
+
+    // Clase Caretaker que es responsable de mantener y gestionar los momentos (Mementos).
+    class Caretaker
+    {
+        private List<Memento> momentos = new List<Memento>();
+
+        public void AgregarMemento(Memento memento)
+        {
+            momentos.Add(memento);
+        }
+
+        public Memento ObtenerMemento(int indice)
+        {
+            if (indice >= 0 && indice < momentos.Count)
+            {
+                return momentos[indice];
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+
+    class main
+    {
+        static void Main(string[] args)
+        {
+            // Crear el objeto de origen y el caretaker.
+            Origen origen = new Origen("Estado Inicial");
+            Caretaker caretaker = new Caretaker();
+
+            // Guardar el estado inicial en el caretaker.
+            caretaker.AgregarMemento(origen.Guardar());
+
+            // Cambiar el estado del origen.
+            origen.SetEstado("Nuevo Estado");
+
+            // Guardar el nuevo estado en el caretaker.
+            caretaker.AgregarMemento(origen.Guardar());
+
+            // Restaurar el estado original desde el caretaker.
+            origen.Restaurar(caretaker.ObtenerMemento(0));
+
+            Console.WriteLine("Estado actual: " + origen.GetEstado());
+        }
+    }
+}
---
+++ TP_Patrones/State.cs
@@ -0,0 +1,68 @@
+﻿using System;
+namespace Patron_State
+{
+    // Interfaz para definir los métodos que deben ser implementados por los estados concretos.
+    interface IEstado
+    {
+        void Manejar();
+    }
+
+    // Clase concreta que representa un estado específico.
+    class EstadoConcretoA : IEstado
+    {
+        public void Manejar()
+        {
+            Console.WriteLine("Manejar el Estado Concreto A");
+        }
+    }
+
+    // Clase concreta que representa otro estado específico.
+    class EstadoConcretoB : IEstado
+    {
+        public void Manejar()
+        {
+            Console.WriteLine("Manejar el Estado Concreto B");
+        }
+    }
+
+    // Clase de contexto que mantiene una referencia al estado actual.
+    class Contexto
+    {
+        private IEstado estadoActual;
+
+        public Contexto()
+        {
+            // Inicializar el estado actual, por ejemplo, a EstadoConcretoA.
+            estadoActual = new EstadoConcretoA();
+        }
+
+        public void CambiarEstado(IEstado nuevoEstado)
+        {
+            estadoActual = nuevoEstado;
+        }
+
+        public void RealizarOperacion()
+        {
+            estadoActual.Manejar();
+        }
+    }
+
+    class State
+    {
+        static void Main(string[] args)
+        {
+            // Crear el contexto.
+            Contexto contexto = new Contexto();
+
+            // Realizar operación en el estado actual.
+            contexto.RealizarOperacion();
+
+            // Cambiar al otro estado.
+            contexto.CambiarEstado(new EstadoConcretoB());
+
+            // Realizar operación en el nuevo estado.
+            contexto.RealizarOperacion();
+        }
+    }
+
+}
